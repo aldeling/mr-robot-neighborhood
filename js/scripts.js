14ba1dbd3a0c2f8413f1ba13b86bf8db947ae747
Athea DeLing <almdeling@gmail.com>
2022-09-09 10:01:12 -0700

    test three pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // Business Logic
 
 function correctInput(number) {
-  if ((number.length === 0) || (!Number(number))); {
-  return "This is not a valid input";
+  if ((number.length === 0) || (!Number(number))) {
+    return "this is not a valid input";
   }
+  return number;
 }
 
 // UI Logic 
