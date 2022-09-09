7362bca3a60ca65a5a4a4ccb2635e5299206fdee
Athea DeLing <almdeling@gmail.com>
2022-09-09 10:44:03 -0700

    test eight pass

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,8 @@ function changeNumber(number) {
     return "Beep!"
   } else if (number === "2") {
     return "Boop!"
+  } else if (number === "3") {
+    return "Won't you be my neighbor!"
   }
 }
 // UI Logic 
