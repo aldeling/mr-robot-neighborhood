58f51faebbfb272e87125531bddbb6e20fa55bb0
Athea DeLing <almdeling@gmail.com>
2022-09-10 14:18:28 -0700

    test 14 pass

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,10 @@ function numberArray(number) {
 function changeNumber(number) {
  if (number.includes("3")) {
     return "Won't you be my neighbor!";
-  } else if (number.includes("1")) {
-    return "Beep!";
   } else if (number.includes("2")) {
     return "Boop!";
+  } else if (number.includes("1")) {
+    return "Beep!";
   }
   return number;
 }
